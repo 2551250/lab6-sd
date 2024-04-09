c74c9dff44589a808fd70737540b6c3a2125d381
2551250 <gdcheadle@gmail.com>
2024-04-09 10:53:44 +0200

    commit

--- api/message/index.js
+++ api/message/index.js
@@ -11,14 +11,12 @@ const cars = require('./cars.json');
 //get all cars
 app.get('/cars', (req, res) => {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.json(cars);
 });
 
 //get car by id
 app.get('/cars/:id', (req, res) => {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header('Access-Control-Allow-Methods', 'GET');
     const id = req.params.id;
     const car = cars.find(car => car.id === parseInt(id));
     res.json(car);
@@ -27,7 +25,6 @@ app.get('/cars/:id', (req, res) => {
 //update car
 app.put('/cars/:id', (req, res) => {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header('Access-Control-Allow-Methods', 'GET, PUT');
     const id = req.params.id;
     const updatedCar = req.body;
     const index = cars.findIndex(car => car.id === id);
@@ -38,7 +35,6 @@ app.put('/cars/:id', (req, res) => {
 //delete car
 app.delete('/cars/:id', (req, res) => {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header('Access-Control-Allow-Methods', 'DELETE');
     const id = req.params.id;
     const index = cars.findIndex(car => car.id === id);
     cars.splice(index, 1);
@@ -48,10 +44,13 @@ app.delete('/cars/:id', (req, res) => {
 //add car
 app.post('/cars', (req, res) => {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header('Access-Control-Allow-Methods', 'POST');
     console.log(req);
     const newCar = req.body;
-    newCar.id = cars[cars.length - 1].id + 1,
+    if (cars.length === 0){
+        newCar.id = 1;
+    }else{
+        newCar.id = cars[cars.length - 1].id + 1;
+    }
     console.log(newCar);
     cars.push(newCar);
     res.json(newCar);
